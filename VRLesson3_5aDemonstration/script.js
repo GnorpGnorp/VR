2e2bcd0a33765cc1432e4de19607258a7b4537b5
GnorpGnorp <danielw194@nycstudents.net>
2025-10-21 14:33:09 +0000

    Really Happy Postman

---
+++ VRLesson3_5aDemonstration/script.js
@@ -0,0 +1,37 @@
+let rnd = (l,u) => Math.random() * (u-l) + l
+let scene, camera, target, ball;
+
+window.addEventListener("DOMContentLoaded",function() {
+  scene = document.querySelector("a-scene");
+  target = document.querySelector("#target");
+  ball = document.querySelector("#ball");
+  ball.x = -1;
+  ball.dx = -0.002;
+  
+  setTimeout(loop,100);
+})
+
+function loop(){
+  
+  ball.x += ball.dx;
+  ball.object3D.position.x = ball.x
+  let d1 = distance(ball, target);
+  output.setAttribute("value",`d1= ${d1}`);
+
+  if(d1 < 1){
+    ball.dx = -ball.dx;
+  }
+  window.requestAnimationFrame(loop);
+}
+
+function distance(obj1,obj2){
+  let x1 = obj1.object3D.position.x;
+  let y1 = obj1.object3D.position.y;
+  let z1 = obj1.object3D.position.z;
+  let x2 = obj2.object3D.position.x;
+  let y2 = obj2.object3D.position.y;
+  let z2 = obj2.object3D.position.z;
+
+  let d = Math.sqrt(Math.pow(x1-x2,2) + Math.pow(y1-y2,2) + Math.pow(z1-z2,2));
+  return d;
+}
